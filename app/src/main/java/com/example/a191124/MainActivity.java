063f5a54065d42d0901089b1966fb8f65b720bfa
shiralevy3 <shiralevy333@gmail.com>
2024-11-19 14:36:40 +0200

    inatial commit close end

---
+++ app/src/main/java/com/example/a191124/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.a191124;
+
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        SharedPreferences sharedPreferences = getSharedPreferences("TestApp", MODE_PRIVATE);
+        TextView tv = findViewById(R.id.tv);
+        tv.setText(sharedPreferences.getString("userprint","no input"));
+        Button btn =findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Are you sure darling")
+                        .setMessage("just make sure this is what you wanttt")
+                        .setPositiveButton("OK",null)
+                        .setNegativeButton("I regret", new di)
+                EditText etd = findViewById(R.id.edi);
+                String name = etd.getText().toString();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                //put data into shared preferences
+                editor.putString("userinput", name);
+                editor.apply();
+
+            }
+        });
+    }
+}
